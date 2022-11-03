309363218a45dce832330e131ef1cfb6e94d9a7b
gorkemu <gorkemunal88@gmail.com>
2022-11-03 03:52:17 +0300

    add tasks to dom

--- src/dom.js
+++ src/dom.js
@@ -1,5 +1,5 @@
 export function displayForm() {
-    document.getElementById("add-task-form-container").style.display = '';    
+    document.getElementById("add-task-form-container").style.display = '';
 }
 
 export function cancelTask() {
@@ -7,3 +7,36 @@ export function cancelTask() {
     document.getElementById("add-task-form-container").style.display = 'none';
 }
 
+export function addTaskToDom(title, description, dueDate, priority) {
+    const todoList = document.querySelector(".todo");
+    const newTodo = document.createElement("div");
+    newTodo.classList.add("new-todo");
+    todoList.appendChild(newTodo);
+
+    const taskTitle = document.createElement("div");
+    taskTitle.textContent = title;
+    newTodo.appendChild(taskTitle);
+
+    const taskDescription = document.createElement("div");
+    taskDescription.textContent = description;
+    newTodo.appendChild(taskDescription);
+
+    const taskDueDate = document.createElement("div");
+    taskDueDate.textContent = dueDate;
+    newTodo.appendChild(taskDueDate);
+
+    const taskPriority = document.createElement("div");
+    taskPriority.textContent = priority;
+    newTodo.appendChild(taskPriority);
+
+    const editTaskButton = document.createElement("button");
+    editTaskButton.textContent = "Edit";
+    editTaskButton.classList.add("edit-task-button");
+    newTodo.appendChild(editTaskButton);
+
+    const deleteTaskButton = document.createElement("button");
+    deleteTaskButton.textContent = "Delete";
+    deleteTaskButton.classList.add("delete-task-button");
+    newTodo.appendChild(deleteTaskButton);
+
+}
