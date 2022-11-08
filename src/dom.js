7b4c5f00b3d43929579ab058731bd0038ae25818
gorkemu <gorkemunal88@gmail.com>
2022-11-08 03:16:40 +0300

    add clearElement function

--- src/dom.js
+++ src/dom.js
@@ -1,42 +1,22 @@
-export function displayForm() {
+export function displayTaskForm() {
     document.getElementById("add-todo-form-container").style.display = '';
 }
-
-export function cancelAddingTodo() {
+export function resetTodoForm() {
     document.getElementById("add-todo-form").reset();
     document.getElementById("add-todo-form-container").style.display = 'none';
 }
 
-export function addTodoToDom(title, description, dueDate, priority) {
-    const todoList = document.querySelector(".todo");
-    const newTodo = document.createElement("div");
-    newTodo.classList.add("new-todo");
-    todoList.appendChild(newTodo);
+export function displayProjectForm() {
+    document.getElementById("new-project-form-container").style.display = '';
+}
 
-    const taskTitle = document.createElement("div");
-    taskTitle.textContent = title;
-    newTodo.appendChild(taskTitle);
+export function resetProjectForm() {
+    document.getElementById("new-project-form").reset();
+    document.getElementById("new-project-form-container").style.display = 'none';
+}
 
-    const taskDescription = document.createElement("div");
-    taskDescription.textContent = description;
-    newTodo.appendChild(taskDescription);
-
-    const taskDueDate = document.createElement("div");
-    taskDueDate.textContent = dueDate;
-    newTodo.appendChild(taskDueDate);
-
-    const taskPriority = document.createElement("div");
-    taskPriority.textContent = priority;
-    newTodo.appendChild(taskPriority);
-
-    const editTodoButton = document.createElement("button");
-    editTodoButton.textContent = "Edit";
-    editTodoButton.classList.add("edit-todo-button");
-    newTodo.appendChild(editTodoButton);
-
-    const deleteTodoButton = document.createElement("button");
-    deleteTodoButton.textContent = "Delete";
-    deleteTodoButton.classList.add("delete-todo-button");
-    newTodo.appendChild(deleteTodoButton);
-
+export function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
 }
