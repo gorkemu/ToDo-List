f920b6039a4d7bbb74af3e2924428e49a780cf38
gorkemu <gorkemunal88@gmail.com>
2022-11-08 03:16:08 +0300

    change form events

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
-import { cancelAddingTodo, displayForm } from './dom';
-import { addTodo } from './todo';
+import { displayProjectForm, displayTaskForm, resetProjectForm, resetTodoForm } from './dom';
+import { createProject, renderProjects } from './projects';
+import { addTodo } from './todos';
 
-const eventListeners = (function() {
+const eventListeners = (() => {
 
-    const addTodoButton = document.querySelector(".add-todo-button");
-    addTodoButton.addEventListener("click", displayForm);
+    const createProjectButton = document.querySelector(".create-project-button");
+    createProjectButton.addEventListener("click", displayProjectForm);
 
-    const cancelButton = document.querySelector(".cancel-button");
-    cancelButton.addEventListener("click", cancelAddingTodo);
+    const cancelProjectButton = document.querySelector(".cancel-project-button");
+    cancelProjectButton.addEventListener("click", resetProjectForm);
 
-    const addButton = document.querySelector(".add-button");
-    addButton.addEventListener("click", addTodo);
+    const createProjectForm = document.querySelector('#new-project-form');
+    createProjectForm.addEventListener('submit', e => {
+        e.preventDefault();
+        createProject();
+    });
+
+    const createTodoButton = document.querySelector(".create-todo-button");
+    createTodoButton.addEventListener("click", displayTaskForm);
+
+    const cancelTodoButton = document.querySelector(".cancel-todo-button");
+    cancelTodoButton.addEventListener("click", resetTodoForm);
+
+    const addTodoForm = document.querySelector("#add-todo-form");
+    addTodoForm.addEventListener("submit", e => {
+        e.preventDefault();
+        addTodo();
+    });
 
     const deleteTodoButton = document.querySelectorAll(".delete-todo-button");
     
     const editTodoButton = document.querySelectorAll(".edit-todo-button");
 })();
+
+// renderProjects();
