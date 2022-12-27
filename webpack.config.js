797e326ef7e53e0daaf435ecf2d3dffc68b2ea52
gorkemu <gorkemunal88@gmail.com>
2022-12-27 15:34:52 +0300

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,20 @@ const path = require("path");
 
 module.exports = {
   mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
   entry: "./src/index.js",
   output: {
     filename: "main.js",
